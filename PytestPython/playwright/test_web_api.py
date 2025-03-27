98067c64baa980f916bc1dd87519d4c0862d622a
Geetansh Kohli <geetanshkohli11@gmail.com>
2025-03-27 10:45:00 +0400

    Update test_web_api.py

--- PytestPython/playwright/test_web_api.py
+++ PytestPython/playwright/test_web_api.py
@@ -15,7 +15,7 @@ def test_e2e_web_api(playwright: Playwright):
     orderId = api_utils.createOrder(playwright)
     #login
     page.goto("https://abc.com/client")
-    page.get_by_placeholder("email@example.com").fill("rahulshetty@gmail.com")
+    page.get_by_placeholder("email@example.com").fill("abc")
     page.get_by_placeholder("enter your passsword").fill("Iamking@000")
     page.get_by_role("button", name="Login").click()
 
